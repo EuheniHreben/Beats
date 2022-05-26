10a6a38f5c74d3e15ba214ff5761b26ff81d974c
EuheniHreben <100788295+EuheniHreben@users.noreply.github.com>
2022-05-26 04:05:26 +0300

    ops

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+const {src, dest, task, series, watch} = require('gulp');
+const rm = require( 'gulp-rm' );
+const sass = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+const sassGlob = require('gulp-sass-glob');
+
+task('clean', () => {
+  return src( 'dist/**/*', { read: false }).pipe(rm());
+});
+
+
+task('copy:html', () => {
+  return src('src/*.html')
+  .pipe(dest('dist'))
+  .pipe(reload({stream:true}));
+});
+
+const styles = [
+  'node_modules/normalize.css/normalize.css',
+  'scss/main.scss'
+]
+
+task('styles', () => {
+  return src(styles)
+  .pipe(concat('main.scss'))
+  .pipe(sassGlob())
+  .pipe(sass().on('error', sass.logError))
+  .pipe(dest('dist'));
+});
+
+task('server', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    },
+    open: false
+  })
+})
+
+watch('./scss/**/*.scss', series('styles'));
+watch('./src/*.html', series('copy:html'));
+task('default', series('clean', 'copy:html', 'styles', 'server'));
